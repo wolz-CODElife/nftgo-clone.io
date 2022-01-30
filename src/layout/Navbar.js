da5f5b2fb3296508c29f804367660e3cfe4d1dac
wolz-CODElife <joeladewole3@gmail.com>
2022-01-30 19:02:56 +0100

    Created media screen breakpoints

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import breakpoint from 'Commons/breakpoints';
+import styled from ''
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+      <FixedTop>
+
+      </FixedTop>
+  )
 };
 
 export default Navbar;
+
+const FixedTop = styled.div`
+    @media only screen and ${breakpoint.device.xs}{
+        display: none;
+    }
+    @media only screen and ${breakpoint.device.sm}{
+        display: flex;
+    }
+    @media only screen and ${breakpoint.device.lg}{
+        display: flex;
+    }
+`;
